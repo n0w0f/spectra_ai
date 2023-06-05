9b6ae6c259d982da61635ff7bd02e9a941589119
nawaf <nawaf.a@qpivolta.tech>
2023-06-05 19:09:15 +0530

    Setup ML cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Ai app to predict optical spectra of a material ',
+    author='nawaf',
+    license='MIT',
+)
