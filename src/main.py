1b965aef436bf6ba614491256ed8834e3437d7b0
nawaf <nawaf.a@qpivolta.tech>
2023-06-07 11:55:55 +0530

    [FIX] made model training configurable via config file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import torch
+import yaml
 from torch_geometric.loader import DataLoader
 
 
@@ -7,13 +8,35 @@ from utils.normalize import normalize_data
 from model.simple_GCN import GCNModel
 from trainer.train import train
 
+
+# Read config from YAML file
+config_path = '/home/nawaf/spectra_ai/src/config/model_config.yaml'
+with open(config_path, 'r') as f:
+    config = yaml.safe_load(f)
+    
 data_list = normalize_data(data_prep.prep_data())
 
 
-loader = DataLoader(data_list, batch_size=1, shuffle=True)
-model = GCNModel(in_channels=4, hidden_channels1=32, hidden_channels2=64, out_channels=128)  # Adjust the input and output channels accordingly
-optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+batch_size = config['batch_size']
+shuffle = config['shuffle']
+in_channels = config['model_params']['in_channels']
+hidden_channels1 = config['model_params']['hidden_channels1']
+hidden_channels2 = config['model_params']['hidden_channels2']
+out_channels = config['model_params']['out_channels']
+learning_rate = config['learning_rate']
+
+
+
+# Create data loader
+loader = DataLoader(data_list, batch_size=batch_size, shuffle=shuffle)
+
+# Create model
+model = GCNModel(in_channels, hidden_channels1, hidden_channels2, out_channels)
+
+# Create optimizer and criterion
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 criterion = torch.nn.MSELoss()
 
-train(model,loader,optimizer,criterion)
+# Train the model
+train(model, loader, optimizer, criterion)
 
